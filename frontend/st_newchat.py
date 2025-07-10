4746e6e7900400f22c56cbadd5ee55b22b6ca1f2
shavonneg <>
2025-07-10 13:47:38 +0800

    Update st_newchat.py

--- frontend/st_newchat.py
+++ frontend/st_newchat.py
@@ -1,9 +1,72 @@
 import streamlit as st
+import requests
 import io
 import base64
 from PIL import Image
 
-from chatbot import chatbot_response
+
+# Initialise chat history
+if "convo_history" not in st.session_state:
+    st.session_state.convo_history = []
+if "awaiting_user" not in st.session_state:
+    st.session_state.awaiting_user = False
+if "last_response" not in st.session_state:
+    st.session_state.last_response = None
+# backend codes
+from chatbot import chatbot_response, initialise_conversation
+
+## update chatbot response in session history
+def on_chat_submit(chat_input):
+    """
+    Handle chat input submissions and interact with the OpenAI API.
+
+    Parameters:
+    - chat_input (str): The chat input from the user.
+
+    Returns:
+    - None: Updates the chat history in Streamlit's session state.
+    """
+    API_URL = "http://127.0.0.1:8000/send_query"
+    user_input = chat_input["text"].strip().lower()
+
+    if 'convo_history' not in st.session_state:
+        st.session_state.convo_history = initialise_conversation()
+
+    payload = {
+            "type": "text",
+            "messages": [user_input]    
+    }
+
+    response = requests.post(
+        API_URL,  # or your deployed FastAPI URL
+        json=payload
+    )
+
+    if response.status_code == 200:
+        response.json()["base64"] = None
+        response.json()["trigger"] = None
+        print(response.json())
+        return response.json()
+    else:
+        print(f"Error: {response.status_code} - {response.text}")
+     
+    # if response.status_code == 200:
+    #     assistant_reply = response.json()["messages"][1]["content"]
+    #     # assistant_reply = response.json() 
+    # else:
+    #     print(f"Error: {response.status_code} - {response.text}")
+    #     assistant_reply = "Sorry, I couldn't process your request at the moment."
+
+    # response = chatbot_response(user_input)
+    # assistant_reply = response
+
+    # st.session_state.convo_history.append({"role": "user", "content": user_input})
+    # st.session_state.convo_history.append({"role": "assistant", "content": assistant_reply})
+
+
+####################################################################################
+# Streamlit app UI - NEW CHAT PAGE
+####################################################################################
 
 ## functions
 def image_to_base64(uploaded_files):
@@ -71,7 +134,7 @@ if user_input := st.chat_input(
     st.chat_message("user").markdown(user_input["text"])
     st.session_state.convo_history.append({"role": "user", "content": user_input['text'], "image": user_input['base64']})
 
-    response = chatbot_response(user_input)
+    response = on_chat_submit(user_input)
     st.session_state.last_response = response
 
     if response.get('trigger', False):
@@ -89,7 +152,7 @@ if st.session_state.awaiting_user:
         with st.chat_message("user"):
             st.markdown(second_input)
             # get the response
-            response = chatbot_response({'text': second_input, 'img': None})
+            response = on_chat_submit({'text': second_input, 'img': None})
             st.session_state.last_response = response
             if response.get('trigger', False):
                 st.session_state.awaiting_user = True
@@ -101,7 +164,7 @@ if st.session_state.awaiting_user:
 if st.session_state.last_response and not st.session_state.awaiting_user:
     response = st.session_state.last_response
     with st.chat_message("assistant"):
-        st.markdown(response['text'])
+        st.markdown(response["messages"][1]["content"])
         if response['base64']:
             images = base64_to_image(response["base64"])
             if len(images) == 1:
@@ -113,5 +176,3 @@ if st.session_state.last_response and not st.session_state.awaiting_user:
                         cols[j].image(each_img, caption=f"Generated Image {i + j + 1}", use_container_width=True)
     st.session_state.convo_history.append({"role": "assistant", "content": response['text'], "image": response["base64"]})
     st.session_state.last_response = None  # Reset after displaying
-
-
