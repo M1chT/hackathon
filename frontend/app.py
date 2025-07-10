87933b23e74943851267601e0b4171a31e97b3d8
Michelle Lee <68367603+michellelsh@users.noreply.github.com>
2025-07-10 10:49:32 +0800

    added trigger and show multiple images

--- frontend/app.py
+++ frontend/app.py
@@ -2,41 +2,11 @@ import streamlit as st
 import base64
 import time
 
-
-### functions for frontend #############################################
-## streamlit - initialise session state
-def initialise_session_state():
-    """Initialise session state variables."""
-    if "history" not in st.session_state:
-        st.session_state.history = []
-    if 'conversation_history' not in st.session_state:
-        st.session_state.conversation_history = []
-    if "logged_in" not in st.session_state:
-        st.session_state.logged_in = False
-
-## login
-def login():
-    if st.button("Login"):
-        st.session_state.logged_in = True
-        st.rerun()
-
-## logout
-def logout():
-    st.session_state["logged_in"] = False
-    st.session_state["user"] = None
-    st.rerun()
-
-## login and logout pages
-login_page = st.Page("login.py", title="Login", icon=":material/login:")
-logout_page = st.Page(logout, title="Log out", icon=":material/logout:")
-
-## other pages
-new_chat = st.Page("pages/st_newchat.py", title="New Chat", icon=":material/chat:", default=True)
-
-
 ####################################################################################
 # Streamlit app UI
 ####################################################################################
+
+new_chat = st.Page("st_newchat.py", title="New Chat", icon=":material/chat:", default=True)
 
 ## hide footer, header, main menu
 hide_st_style = """
@@ -48,30 +18,34 @@ hide_st_style = """
 st.markdown(hide_st_style, unsafe_allow_html=True)
 
 ## page layout
-st.set_page_config(page_title="Product Marketing Studio", 
+st.set_page_config(page_title="PromoGenie", 
                    page_icon="📢",
                    layout='wide', 
                    initial_sidebar_state='auto',
                 )
 
-
 def main():
     """
     Display Streamlit updates and handle the chat interface.
     """
-    initialise_session_state()
 
-    ## navigation (replacement of sidebar with clickable pages)
-    if st.session_state.logged_in:
-        pg = st.navigation(
-            {
-                "Account": [logout_page],
-                "Pages": [new_chat],
-            }
-        )
-    else:
-        pg = st.navigation([login_page])
+    ## sidebar
+    # sidebar for platform descriptions
+    st.sidebar.header("How to use")
+    st.sidebar.markdown('''
+               Blah blah blah
+                ''')
+    st.sidebar.markdown("---")
+    st.sidebar.header("About")
+    st.sidebar.markdown('''
+                ### PromoGenie
+                #### Powered by Agents
 
+                The platform, PMS assists our Operations Manager (OM) to design a marketing strategy to achieve product's marketing goals through various tools (e.g., competitive analysis, marketing knowledge base, collaterals generator).
+                ''')
+    
+    ## new chat page
+    pg = st.navigation([new_chat])
     pg.run()
 
 
--- frontend/chatbot.py
+++ frontend/chatbot.py
@@ -1,34 +1,79 @@
-## backend
+## backend codes
+import io
+import base64
+from PIL import Image
+
+# convert image to base64
+def image_to_base64(uploaded_files):
+    # uploaded_files is a list of UploadedFile objects from Streamlit
+    base64_list = []
+    for uploaded_file in uploaded_files:
+        image = Image.open(uploaded_file)
+        buffered = io.BytesIO()
+        image.save(buffered, format="PNG")
+        img_str = base64.b64encode(buffered.getvalue()).decode()
+        base64_list.append(img_str)
+    return base64_list
+
 # Simple rule-based chatbot logic
 def chatbot_response(user_input):
-    user_input = user_input.lower()
-    if "hello" in user_input or "hi" in user_input:
-        return "Hello! How can I help you today?"
-    elif "how are you" in user_input:
-        return "I'm just a bot, but I'm doing great! Thanks for asking."
-    elif "bye" in user_input:
-        return "Goodbye! Have a great day."
+    """
+    Parameter:
+        user_input: {
+        'text': user query in text format,
+        'img': user's uploaded img}
+
+    Output:
+        response: {
+        'text': text response, 
+        'base64': img in base64}
+    """
+    query = user_input['text'].lower()
+    response = {"trigger": False, "text": "", "base64": None}
+
+    if "hello" in query or "hi" in query:
+        response['text'] = "Hello! How can I help you today?"
+        response['base64'] = None
+        return response
+    elif "how are you" in query:
+        response['text'] = "I'm just a bot, but I'm doing great! Thanks for asking."
+        response['base64'] = None
+        return response
+    elif "bye" in query:
+        response['text'] = "Goodbye! Have a great day."
+        response['base64'] = None
+        return response
+    elif "trigger" in query:
+        response['text'] = None
+        response['base64'] = None
+        response['trigger'] = True
+        return response
+    elif "accept" in query:
+        response['text'] = "tool triggered"
+        response['base64'] = None
+        response['trigger'] = False
+        return response
+    elif "rewrite" in query:
+        response['text'] = "ok got your rewritten stuff"
+        response['base64'] = None
+        response['trigger'] = False
+        return response
+    elif "reject" in query:
+        response['text'] = "ok you asked me not to execute"
+        response['base64'] = None
+    elif "more images" in query:
+        response['text'] = "I hope the generated image fits your needs"
+        response['base64'] = image_to_base64(["./images/31941-Christmas-New-Year-snow-winter-snowman-4K.jpg", "./images/790317-snowman-snow-xmas-christmas-figure-cinnamon-4K.jpg"])
+        return response
+    elif "three options" in query:
+        response['text'] = "I hope the generated image fits your needs"
+        response['base64'] = image_to_base64(["./images/31941-Christmas-New-Year-snow-winter-snowman-4K.jpg", "./images/790317-snowman-snow-xmas-christmas-figure-cinnamon-4K.jpg", "./images/snow_covered_road_and_trees_in_winter_4k_hd_nature-1920x1080.jpg"])
+        return response
+    elif "generate" in query:
+        response['text'] = "I hope the generated image fits your needs"
+        response['base64'] = image_to_base64(["./images/31941-Christmas-New-Year-snow-winter-snowman-4K.jpg"])
+        return response
     else:
-        return "I'm not sure how to respond to that. Can you rephrase?"
-    
-
-## initialise conversation
-def initialise_conversation():
-    """
-    Initialise the conversation history with system and assistant messages.
-
-    Returns:
-    - list: Initialised conversation history.
-    """
-    assistant_message = "Hello! I am your marketing guru. How can I assist you with your product today?"
-
-    # lol this serves as a prompt
-    conversation_history = [
-        {"role": "system", "content": "You are Streamly, a specialized AI assistant trained in Streamlit."},
-        {"role": "system", "content": "Streamly, is powered by the OpenAI GPT-4o-mini model, released on July 18, 2024."},
-        {"role": "system", "content": "You are trained up to Streamlit Version 1.36.0, release on June 20, 2024."},
-        {"role": "system", "content": "Refer to conversation history to provide context to your response."},
-        {"role": "system", "content": "You were created by Madie Laine, an OpenAI Researcher."},
-        {"role": "assistant", "content": assistant_message}
-    ]
-    return conversation_history
+        response['text'] = "I'm not sure how to respond to that. Can you rephrase?"
+        response['base64'] = None
+        return response
